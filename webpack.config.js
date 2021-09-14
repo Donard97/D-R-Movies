53d1158263eea7ac98934704796ddf5cec5cddfc
Donard97 <donardgolaj@gmail.com>
2021-09-14 17:11:45 +0200

    Add footer and style to it

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Javascript-Capstone-Project',
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/JavaScript-Capstone-Project/',
   },
   module: {
     rules: [
